1172ad8f787d14b40bba5cfbdb31e4ab48b4036f
NitrousPrawn95 <125307541+NitrousPrawn95@users.noreply.github.com>
2023-12-08 09:03:31 -0700

    Create Shenanigan.java

---
+++ Shenanigan.java
@@ -0,0 +1,11 @@
+public class Shenanigan {
+    private String name;
+    private int moves;
+    String shenanigan;
+    public Shenanigan() {
+name = "bob";
+moves = -1;
+shenanigan = "null";
+    }
+
+}
